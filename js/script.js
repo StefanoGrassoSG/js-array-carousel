b29149d8b7dda56777889f67aa3c21550c560075
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-16 17:58:07 +0200

    created elements in js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//ARRAY
+
+const imgList = [
+    '<img src="img/01.webp" alt="">',
+    '<img src="img/02.webp" alt="">',
+    '<img src="img/03.webp" alt="">',
+    '<img src="img/04.webp" alt="">',
+    '<img src="img/05.webp" alt="">'
+]
+
+console.log(imgList)
+
+for (let index = 0; index < imgList.length; index++) {
+    const element = imgList[index];
+    const item = document.createElement('div');
+    item.innerHTML = [index];
+    document.querySelector('.items').appendChild(item);
+    
+}
