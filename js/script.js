07e5cbc292a3abc9f97049d30206d991e8482920
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-17 16:17:31 +0200

    add switch class in js

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ const imgList = [
 
 console.log(imgList)
 let next = document.getElementById("go")
+let back = document.getElementById("down")
 
 
 for (let index = 0; index < imgList.length; index++) {
@@ -20,11 +21,58 @@ for (let index = 0; index < imgList.length; index++) {
     item.classList.add('img');   
 }
 
-let imgItems = document.getElementsByClassName('img');
+let imgItems = document.querySelectorAll('.img');
 let firstItem = imgItems[0];
-firstItem.className += ' active';
+firstItem.classList.add('active')
 
+next.addEventListener('click', function(){
 
+    let current = document.querySelector('.active')
+
+    if(current.classList.contains('active')){
+        imgItems[1].classList.add('active')
+        imgItems[0].classList.remove('active')
+    }
+
+    if (current.classList.contains('active')) {
+        imgItems[1].classList.remove('active')
+        imgItems[2].classList.add('active')
+    }
+    if (current.classList.contains('active')) {
+        imgItems[2].classList.remove('active')
+        imgItems[3].classList.add('active')
+    }
+    if (current.classList.contains('active')) {
+        imgItems[3].classList.remove('active')
+        imgItems[4].classList.add('active')
+    }
+    
+
+})
+
+back.addEventListener('click', function(){
+
+    let current = document.querySelector('.active')
+
+    if(current.classList.contains('active')){
+        imgItems[3].classList.add('active')
+        imgItems[4].classList.remove('active')
+    }
+
+    if (current.classList.contains('active')) {
+        imgItems[2].classList.add('active')
+        imgItems[3].classList.remove('active')
+    }
+    if (current.classList.contains('active')) {
+        imgItems[1].classList.add('active')
+        imgItems[2].classList.remove('active')
+    }
+    if (current.classList.contains('active')) {
+        imgItems[0].classList.add('active')
+        imgItems[1].classList.remove('active')
+    }
+
+})
 
 
 
